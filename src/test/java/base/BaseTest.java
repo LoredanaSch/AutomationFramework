a8c90247f9db55402517c54f8d99525b98f4ccc5
LoredanaSch <loredana_schiopu90@yahoo.com>
2022-08-23 13:41:59 +0300

    Fix GIT

--- src/test/java/base/BaseTest.java
+++ src/test/java/base/BaseTest.java
@@ -1,9 +1,11 @@
 package base;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
 import org.junit.After;
 import org.junit.Before;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 
 public class BaseTest {
 
@@ -11,8 +13,14 @@ public class BaseTest {
 
    // @Before
     public void Setup(){
-        System.setProperty("webdriver.chrome.driver", "src/test/resources/chromedriver");
-        driver = new ChromeDriver();
+        //System.setProperty("webdriver.chrome.driver", "src/test/resources/chromedriver");
+        ChromeOptions chromeOptions = new ChromeOptions();
+        chromeOptions.addArguments("--headless");
+        chromeOptions.addArguments("--disable-gpu");
+        chromeOptions.addArguments("--window-size=1920,1080");
+        chromeOptions.addArguments("--no-sandbox");
+        WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver(chromeOptions);
         driver.get("https://demo.automationtesting.in/Index.html");
         driver.manage().window().maximize();
     }
