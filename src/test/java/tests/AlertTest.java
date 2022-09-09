63c75ec23e971e13556fd0a8f34385a5801aed13
LoredanaSch <loredana_schiopu90@yahoo.com>
2022-09-09 12:56:08 +0300

    all

--- src/test/java/tests/AlertTest.java
+++ src/test/java/tests/AlertTest.java
@@ -6,18 +6,7 @@ import Pages.AlertPage;
 import Pages.IndexPage;
 import Pages.RegisterPage;
 import base.Hooks;
-import helpMethods.AlertMethods;
-import helpMethods.ElementMethods;
-import helpMethods.FrameMethods;
-import helpMethods.PageMethods;
-import base.BaseTest;
 import org.junit.Test;
-import org.openqa.selenium.Alert;
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebElement;
-
-import java.sql.Driver;
-import java.util.List;
 
 public class AlertTest extends Hooks {
 
